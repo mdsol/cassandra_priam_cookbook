a08612287a9237fcb5e2c628d32ce6060b43a83a
atrullmdsol <atrull@mdsol.com>
2013-10-03 12:57:03 +0200

    Bumped version due to some significant changes.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@mdsol.com"
 license          "Apache 2.0"
 description      "Installs Priam-managed Cassandra 1.2 along with Oracle Java and Datastax OpsCenter"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.1"
+version "0.0.2"
 
 depends "runit"
 depends "nginx_proxy" # For DataStax OpsCenter
