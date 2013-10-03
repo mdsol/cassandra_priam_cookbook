31252945615cb94488200a884da65f2c3991e561
atrullmdsol <atrull@mdsol.com>
2013-10-03 16:50:25 +0200

    Removed opscenter attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,6 @@ default[:cassandra][:log_dir] = "/var/log/cassandra" # where it this set or used
 # datastax cassandra comes prefixed dsc-cassandra - modify as necessary for your needs
 default[:cassandra][:nameprefix] = "dsc-cassandra"
 default[:cassandra][:parentdir] = "/opt"
-default[:cassandra][:opscenter_home] = "#{node[:cassandra][:parentdir]}/opscenter"
 
 # Tomcat defaults for Ubuntu - these are needed by Priam
 default[:tomcat][:webappsroot] = "/var/lib/tomcat7/webapps"
@@ -28,18 +27,14 @@ default[:cassandra][:priam_cass_stopscript] = "/etc/init.d/cassandra stop"
 default[:cassandra][:priam_upload_throttle] = "5"
 
 # This cookbooks requires a LOT of software to be stored in an http server.
-# Source files are in a Medidata-controlled S3 bucket with open http auth
+# Source files are in a Medidata-controlled S3 bucket with no authentication
 SRC = 'http://cloudteam-packages.s3.amazonaws.com/cassandra'
 # you can roll your own as necessary.
-# /cassandra/ (cassandra and opscenter files including an extracted agent tar.gz)
 # /cassandra/priam/priam-version/ (priam files)
 # checksums are sha256 - the default chef remote_file checksum - obviously these need to be modified for later versions of software
 default[:cassandra][:version] = "1.2.10"
 default[:cassandra][:src_url] = "http://downloads.datastax.com/community/#{node['cassandra']['nameprefix']}-#{node['cassandra']['version']}-bin.tar.gz"
 default[:cassandra][:checksum] = "c731c8e2bc84769f884f423fb839ab3205279972b842ab37fdace49ef511e544"
-default[:cassandra][:opscenter][:version] = "3.2.2"
-default[:cassandra][:opscenter][:src_url] = "http://downloads.datastax.com/community//opscenter-#{node['cassandra']['opscenter']['version']}-free.tar.gz"
-default[:cassandra][:opscenter][:checksum] = "568b9e8767a0ed1bc7f101f39cf400f63fbba4f7dceefafab19c608aaf386950"
 default[:cassandra][:priam_version] = "1.2.17"
 default[:cassandra][:priam_web_war][:src_url] = "#{SRC}/priam/#{node['cassandra']['priam_version']}/priam-web-#{node['cassandra']['priam_version']}.war"
 default[:cassandra][:priam_web_war][:checksum] = "fbc1779f9cff9a8e3a4933000a9f2784c2037519f0e1f2777ae39dfee9d831a0"
