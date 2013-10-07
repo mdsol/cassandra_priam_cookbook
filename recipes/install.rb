4d5c0327f8615183d0f172b062bbfc15764a2ed8
atrullmdsol <atrull@mdsol.com>
2013-10-07 12:23:27 +0200

    Corrected reporting variables for leader election.

--- recipes/install.rb
+++ recipes/install.rb
@@ -23,7 +23,7 @@ peers = search(:node, "roles:#{node[:roles].first}" )
 leader = peers.sort{|a,b| a.uptime_seconds <=> b.uptime_seconds}.last || node    # the "or" covers the case where node is the first db
 
 # Some reporting on the election
-log "cassandra-priam LeaderElection: #{node[:roles].first} Leader is : #{$LEADERNAME} #{$LEADEREC2PUBLICHOSTNAME} #{$LEADERIPADDRESS} "
+log "cassandra-opscenter LeaderElection: #{node[:roles].first} Leader is : #{leader.name} #{leader.ec2.public_hostname} #{leader.ipaddress}"
 
 if (node.name == leader.name)
   # Apply the Priam/Cassandra configuration to Amazon SDB
