a481d3eb12fdf1e6adabc9cbeed990cd26608730
atrullmdsol <atrull@mdsol.com>
2013-10-14 13:26:26 +0200

    Bumping version and setting the correct name in metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             'cassandra-priam'
 maintainer       "Medidata Solutions, Inc."
 maintainer_email "cookbooks@mdsol.com"
 license          "Apache 2.0"
 description      "Installs Priam-managed Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.4"
+version "0.0.5"
 
 depends "sudo"
 depends "runit"
