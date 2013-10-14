d5b813d56ef576885f033962154d9b677ab25bba
atrullmdsol <atrull@mdsol.com>
2013-10-14 13:30:55 +0200

    Added the support of OSes we expect should work.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,10 @@ description      "Installs Priam-managed Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.0.5"
 
+%w{ ubuntu debian redhat fedora centos scientific amazon }.each do |os|
+  supports os
+end
+
 depends "sudo"
 depends "runit"
 depends "java"
